7fc2ac91ce4ca5d12a175fd3312ac2fdc70fd656
rogercodes1 <rogerperez.us@gmail.com>
2018-11-01 11:36:54 -0700

    added code.

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("hello")
+var foo = document.getElementById('foo');
+
+// write a method that returns the value "quux"
+function returnCustomAttr() {
+    return foo.getAttribute(("data-bar"))
+}
+
+// write another method that returns the value "quux"
+function returnCustomAttr2() {
+    return foo.dataset.bar
+}
+
+// write yet another method that retuns the value "quux"
+function returnCustomAttr3() {
+
+}
+
+document.body.innerHTML = returnCustomAttr() + ' <br/>' + returnCustomAttr2() + ' <br/>' + returnCustomAttr3()
