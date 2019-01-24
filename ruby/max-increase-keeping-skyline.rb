adf0365937f37f089525ef4f4d8303849bf4e52c
rogercodes1 <rogerperez.us@gmail.com>
2019-01-24 08:57:37 -0800

    required pry and added .each code.

--- ruby/max-increase-keeping-skyline.rb
+++ ruby/max-increase-keeping-skyline.rb
@@ -1,13 +1,17 @@
+require "pry"
 grid = [[3,0,8,4],[2,4,5,7],[9,2,6,3],[0,3,1,0]]
 
 def max_increase_keeping_skyline(grid)
-    largest_lr_num = 0
-    largest_tb_num = 0
+    largest_lr_num_arr = []
+    largest_tb_num_arr = []
 #     loop thru array of arrays
 #     determine the largest number in the first array and save index.
-    grid.each do |num|
+    grid.each do |arr|
+      largest_lr_num_arr << arr.max
+
+      binding.pry
 
     end
-#     grid
 
 end
+max_increase_keeping_skyline(grid)
