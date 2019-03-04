94652bc361b0c88dff0a71093a1e60cdfca6081a
rogercodes1 <rogerperez.us@gmail.com>
2019-03-03 20:58:40 -0800

    working solution. Not the fastest.

--- javascript/n-rep-elem-in-2N-Arr.js
+++ javascript/n-rep-elem-in-2N-Arr.js
@@ -1,17 +1,27 @@
 // console.log("2N-Arr");
-
+// let arr = [2,1,2,5,3,2];
+let arr2 = [5,1,5,2,5,3,5,4];
 let repeatedNTimes = function(A) {
-    console.log("A: ",A)
-    let countNumObj = {};
 
+    let countNumObj = {};
+    let numKey, value = 0;
     A.forEach(num =>{
-        console.log("NUM: ", num)
-        console.log("CNO:", countNumObj.length)
-        if (countNumObj.length === 0 || !countNumObj.keys.includes(num)) {
-            countNumObj[num]=1
-        } else {
-            countNumObj +=1
-        }
+        (!countNumObj.hasOwnProperty(num))
+        ? countNumObj[num]=1
+        : countNumObj[num] +=1
     })
-    console.log("CNO: ",countNumObj)
+    // Commented out code using reduce returns largest value. Works. Alt sol.
+    // let finalKey = Object.keys(countNumObj).reduce((a, b) => countNumObj[a] > countNumObj[b] ? a : b);
+    // MySOLUTION:
+    let otherKey = Object.keys(countNumObj).forEach((key)=>{
+      if (countNumObj[key]> value){
+        numKey = key
+        value = countNumObj[key]
+      }
+    });
+    console.log("numKey: ", numKey);
+    return numKey
+    debugger
 };
+
+repeatedNTimes(arr2)
