3bd5234f7ec44388d48142ea6ad6e3c73024b88e
rogercodes1 <rogerperez.us@gmail.com>
2019-03-03 21:01:51 -0800

    added fastest solution.

--- javascript/n-rep-elem-in-2N-Arr.js
+++ javascript/n-rep-elem-in-2N-Arr.js
@@ -1,7 +1,7 @@
 // console.log("2N-Arr");
 // let arr = [2,1,2,5,3,2];
 let arr2 = [5,1,5,2,5,3,5,4];
-let repeatedNTimes = function(A) {
+let repeatedNTimesSlowest = function(A) {
 
     let countNumObj = {};
     let numKey, value = 0;
@@ -22,6 +22,35 @@ let repeatedNTimes = function(A) {
     console.log("numKey: ", numKey);
     return numKey
     debugger
+};
+
+// 100% faster****************
+var repeatedNTimes = function (A) {
+    let letterMap = new Map();
+    for (let elem of A) {
+        if (letterMap.has(elem)) {
+                return elem;
+                break;
+        } else {
+            letterMap.set(elem, 1);
+        }
+    }
+};
+
+
+var repeatedNTimes = function(A) {
+    let uniqueTracker = {};
+    for (const el of A) {
+        if (uniqueTracker[el]) {
+            return el;
+        } else {
+            uniqueTracker[el] = 1;
+        }
+        console.log(uniqueTracker);
+
+    }
+    console.log(uniqueTracker);
+    return null;
 };
 
 repeatedNTimes(arr2)
