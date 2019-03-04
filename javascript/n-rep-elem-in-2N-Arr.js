c899e5a98c93662f31eb33d9f08f480ed85fe764
github username <github email address>
2019-03-03 20:11:39 -0800

    added new file of n-rep-elem-in-2N-Arr

---
+++ javascript/n-rep-elem-in-2N-Arr.js
@@ -0,0 +1,17 @@
+// console.log("2N-Arr");
+
+var repeatedNTimes = function(A) {
+    console.log("A: ",A)
+    let countNumObj = {};
+
+    A.forEach(num =>{
+        console.log("NUM: ", num)
+        console.log("CNO:", countNumObj.length)
+        if (countNumObj.length === 0 || !countNumObj.keys.includes(num)) {
+            countNumObj[num]=1
+        } else {
+            countNumObj +=1
+        }
+    })
+    console.log("CNO: ",countNumObj)
+};
