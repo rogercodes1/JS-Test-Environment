00865b3e250215476a3624284ba3540ce75bbdc3
Roger Perez <rogerperez.us@gmail.com>
2019-06-09 17:30:06 -0700

    better check

--- javascript/telephoneCHECK.js
+++ javascript/telephoneCHECK.js
@@ -19,7 +19,11 @@ function phoneCheck(str) {
   }
   return false;
 }
-
+// Better alternate
+function telephoneCheck(str) {
+   var regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+   return regex.test(str);
+}
 
 telephoneCheck("555-555-5555");
 telephoneCheck("1 555-555-5555")
