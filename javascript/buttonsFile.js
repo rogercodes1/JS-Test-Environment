80f64a5edd898929c4f45dd26316d8df83162607
rogercodes1 <rogerperez.us@gmail.com>
2019-06-29 17:40:41 -0700

    updated solution.

--- javascript/buttonsFile.js
+++ javascript/buttonsFile.js
@@ -1,19 +1,15 @@
-console.log("HEREEE");
-
 let btnCount = 20
-
 let mainDiv = document.getElementById("main")
 
 for (let i = 0; i< btnCount; i++){
     let btnNum = document.createElement("BUTTON")
     btnNum.id = i+1
     btnNum.innerHTML = `Btn: ${i+1}`
+    btnNum.addEventListener("click", function(e){
+      let btnNum = e.target.id
+      alert(`Button ${btnNum} was clicked!`)
+    })
     document.body.appendChild(btnNum)
     mainDiv.appendChild(btnNum)
 
 }
-
-mainDiv.addEventListener("click", function(e){
-  let btnNum = e.target.id
-  alert(`Button ${btnNum} was clicked!`)
-})
