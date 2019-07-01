6ec04995e7fcddd7223714213d5fee6c414b770a
rogercodes1 <rogerperez.us@gmail.com>
2019-06-30 21:28:00 -0700

    updated solution.

--- javascript/shortestSubstring.js
+++ javascript/shortestSubstring.js
@@ -5,7 +5,8 @@ let ss = "dabbacbcd"
 
 // output = abcd
 
-
+// solution runs in O(ab) time. Meaning the first while loop is the first O(n) and
+// b is the time of the second while loop which is not the same size of the first loop.
 function shortestSubstring(s){
   let start, end, count;
   let arr = s.split("")
@@ -15,17 +16,14 @@ function shortestSubstring(s){
   let result = s; // sets the answer to the full string to compare to shorter minimal string
 
   while (i < arr.length ){
-    let strCheck = arr.slice(i,charLen + i)
-    let str = strCheck.join("")
+    let strCheck = arr.slice(i,charLen + i).join("")
     count = 0;
     let j = 0
     while ( j < charValArr.length){
-      if (!str.includes(charValArr[j])){
-        break
-      }
+      if (!strCheck.includes(charValArr[j])){ break }
       count++
       start = i
-      end = i + str.length
+      end = i + strCheck.length
       if (count == charValArr.length){
         break
       }
@@ -33,21 +31,23 @@ function shortestSubstring(s){
         j++
       }
     } // end of while
-    let temp = arr.slice(start,end).join("")
-    if (count === charValArr.length && temp.length < result.length ){
-      result = temp
+    let currStr = arr.slice(start,end).join("")
+    if (count === charValArr.length && currStr.length < result.length ){
+      result = currStr
       break
     }
+    // if resets back to i === 0 and starts validating once more by adding a new char to string comparison
     if (i === (arr.length - charLen)) {
       i = -1
       charLen+=1
     }
     i++
   }
-  return result
+  return result.length
 
 }
 
+// generates the unique chars/letters needed to compare to find a solution.
 function generateArrayOfChars(arr){
   let charObj = {}
   for (let i = 0; i<arr.length; i++){
