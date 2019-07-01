4cef80a651562ecede7550ca437335574e0338ce
rogercodes1 <rogerperez.us@gmail.com>
2019-06-30 21:12:47 -0700

    updated solution. Working

--- javascript/shortestSubstring.js
+++ javascript/shortestSubstring.js
@@ -1,24 +1,52 @@
-console.log("SHORT");
+// console.log("SHORT");
 
 let s = "dabbcabcd"
+let ss = "dabbacbcd"
+
 // output = abcd
 
 
 function shortestSubstring(s){
+  let start, end, count;
   let arr = s.split("")
   let charValArr = generateArrayOfChars(arr)
   let charLen = charValArr.length
   let i = 0;
+  let result = s; // sets the answer to the full string to compare to shorter minimal string
 
-  while (i< arr.length){
-    let strCheck = arr.slice(i,charLen-1)
-    console.log("S: ",strCheck);
+  while (i < arr.length ){
+    let strCheck = arr.slice(i,charLen + i)
+    let str = strCheck.join("")
+    count = 0;
+    let j = 0
+    while ( j < charValArr.length){
+      if (!str.includes(charValArr[j])){
+        break
+      }
+      count++
+      start = i
+      end = i + str.length
+      if (count == charValArr.length){
+        break
+      }
+      else {
+        j++
+      }
+    } // end of while
+    let temp = arr.slice(start,end).join("")
+    if (count === charValArr.length && temp.length < result.length ){
+      result = temp
+      break
+    }
+    if (i === (arr.length - charLen)) {
+      i = -1
+      charLen+=1
+    }
+    i++
   }
-  console.log("C:", charValArr);
-  debugger
+  return result
 
 }
-shortestSubstring(s)
 
 function generateArrayOfChars(arr){
   let charObj = {}
@@ -29,3 +57,5 @@ function generateArrayOfChars(arr){
   }
   return Object.keys(charObj)
 }
+
+shortestSubstring(ss)
