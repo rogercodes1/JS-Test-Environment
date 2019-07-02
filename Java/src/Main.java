6589347c7bc41809c27001d9ec69645c2f67598d
rogercodes1 <rogerperez.us@gmail.com>
2019-07-02 14:10:15 -0700

    save new code.

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -3,6 +3,21 @@ import java.awt.*;
 public class Main {
 
     public static void main(String[] args) {
+
+
+        int[] numbers = new int[5];
+
+        numbers[0] = 20;
+        numbers[1] = 90;
+        numbers[2] = 28;
+        numbers[3] = 40;
+        numbers[4] = 77;
+
+        String[] newStr = new String[2];
+        newStr[0] = "Bitter Cup";
+
+        System.out.println("Array: " + numbers[3]);
+        System.out.println("StrArr: " + newStr[0]);
         System.out.println("Roger - Hello World!!");
 
         Car myCar = new Car(25.5, "1BCED45", Color.BLUE, true);
